8f3406d872f474c424d0e7a82f9cc9ffd57cb687
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-09-08 18:29:23 +0900

    Initial commit

---
+++ casa_piperestorescript.py
@@ -0,0 +1,6 @@
+__rethrow_casa_exceptions = True
+h_init()
+try:
+    hifa_restoredata (vis=['uid___A002_Xc27dd7_X2fc3', 'uid___A002_Xc27dd7_X6983', 'uid___A002_Xc27dd7_X6c8e', 'uid___A002_Xc2b007_X877', 'uid___A002_Xc8592e_Xaea3', 'uid___A002_Xc89480_X8c7d', 'uid___A002_Xc96463_X1646', 'uid___A002_Xc96f17_X113a'], session=['session_1', 'session_2', 'session_2', 'session_3', 'session_4', 'session_5', 'session_7', 'session_8'], ocorr_mode='ca')
+finally:
+    h_save()
